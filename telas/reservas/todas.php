01d0e1dfc12f521b2f8a55161494e6572ac4e301
giancunha <wgaweb@gmail.com>
2017-08-20 23:10:39 -0300

    Reservas - Listagem todas, controle de acesso a edição e exclusão pelo reservante.

--- telas/reservas/todas.php
+++ telas/reservas/todas.php
@@ -1,3 +1,12 @@
+<?php 
+    if(!isset($_POST['data'])){
+        $data = date('d/m/Y');
+    } else {
+        $data = $_POST['data'];
+    }
+    $usuario = unserialize($_SESSION['usuario_adm_'.SESSAOADM]);
+    $idUsuario = $usuario->getIdUsuario();
+?>
 <h1>Todas Reservas</h1>
 <div class="table-responsive">
     <table>
@@ -9,7 +18,7 @@
                 <th>Responsável</th>
                 <th>Sala</th>
                 <th>Descrição</th>
-                <th>&nbsp;</th>
+                <th colspan="2">&nbsp;</th>
             </tr>
         </thead>
         <tbody>
@@ -30,9 +39,24 @@
                 <td><?php echo $usuario->getNome(); ?></td>
                 <td><?php echo $sala->getNome(); ?></td>
                 <td><?php echo $valor->getDescricao(); ?></td>
+    <?php
+        if($idUsuario == $valor->getIdUsuario()){
+    ?>
                 <td style="text-align:right">
                     <a href="<?php echo URL."/".$gets[0]."/edita".ucfirst($gets[0]); ?>/<?php echo $valor->getIdReserva(); ?>">Editar</a>
                 </td>
+                <td style="text-align:right">
+                    <a href="<?php echo URL."/".$gets[0]."/exclui".ucfirst($gets[0]); ?>/<?php echo $valor->getIdReserva(); ?>" onclick="return duvida('Realmente deseja excluir essa reserva?');">Excluir</a>
+                </td>
+    <?php
+        } else {
+    ?>
+                <td colspan="2">
+                    &nbsp;
+                </td>
+    <?php
+        }
+    ?>
             </tr>
     <?php
     }
